2e18f282ee6d0681f4a1bb93cf11f891d76a2021
Tim Cassidy <tcassidy@ea.com>
2019-06-25 16:23:36 -0700

    routes: set up three routes: added dashboard, set up routes for dashboard (default), heroes + details for specific heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -23,4 +23,11 @@ export class HeroService {
 
     return of(heroes);
   }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
 }
